13221d7b58e651910f5f40e1c24ecee425d6a300
Logan Stucki <loganstucki@gmail.com>
2018-08-18 12:39:02 -0500

    First code commit, basic reading functionality

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Constants } from 'expo';
+
+import Reader from './components/Reader';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Reader />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight
+  },
+});
---
+++ components/Reader.js
@@ -0,0 +1,134 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+import { Button, Slider } from 'react-native-elements';
+
+export default class Reader extends React.Component {
+  state = {
+    word: null,
+    pause: true,
+    wpm: 250
+  }
+
+  index = 0
+  paragraph = "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then".split(" ")
+
+  componentDidMount() {
+    if (!this.interval) {
+      this.interval = setInterval(this.nextWord.bind(this), 1 / (this.state.wpm/ 60) * 1000 )
+    }
+
+    if (this.state.word === null) {
+        this.setState({ word: this.paragraph[this.index++] })
+    }
+  }
+
+  nextWord() {
+    if (!this.state.pause) {
+      this.setState({ word: this.paragraph[this.index++] })
+    }
+  }
+
+  pause() {
+    this.setState({ pause: true });
+  }
+
+  resume() {
+    this.setState({ pause: false });
+  }
+
+  back() {
+    var i;
+
+    for (i = this.index - 2; i >= 0; i--) {
+      if (this.paragraph[i].endsWith('.')) {
+        this.index = i + 1;
+        break;
+      }
+    }
+
+    if (i <= -1) {
+      this.index = 0;
+    }
+
+    this.setState({ word: this.paragraph[this.index] })
+  }
+
+  forward() {
+    var i;
+
+    for (i = this.index; i < this.paragraph.length; i++) {
+      if (this.paragraph[i].endsWith('.')) {
+        this.index = i + 1;
+        break;
+      }
+    }
+    
+    this.setState({ word: this.paragraph[this.index] });
+  }
+
+  updateWPM(wpm) {
+    ms = 1 / (wpm / 60) * 1000;
+
+    clearInterval(this.interval);
+    this.interval = setInterval(this.nextWord.bind(this), ms)
+
+    this.setState({ wpm });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>
+          {this.state.word}
+        </Text>
+        
+        <View style={styles.buttonBar}>
+          <Button title="Back" onPress={this.back.bind(this)} />
+          {!this.state.pause?
+            <Button title="Pause" onPress={this.pause.bind(this)} />
+          :
+            <Button title="Start" onPress={this.resume.bind(this)} />
+          }
+          <Button title="Forward" onPress={this.forward.bind(this)} />
+        </View>
+
+        <View style={styles.slider}>
+          <Text>
+            WPM: {this.state.wpm}
+          </Text>
+
+          <Slider 
+            minimumValue={50}
+            maximumValue={1000}
+            step={50}
+            value={this.state.wpm}
+            onSlidingComplete={this.updateWPM.bind(this)}
+            
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+  buttonBar: {
+    flexDirection: 'row'
+  },
+  slider: {
+    flex: 1, 
+    alignItems: 'stretch', 
+  }
+});
