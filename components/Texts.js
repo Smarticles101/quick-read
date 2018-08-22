7b80e449006ea5855d31e963afac656fa10e67b8
Logan Stucki <loganstucki@gmail.com>
2018-08-21 20:23:18 -0500

    Updates to Texts.js

--- components/Texts.js
+++ components/Texts.js
@@ -1,55 +1,88 @@
 import React from 'react';
 import { StyleSheet, ScrollView, View } from 'react-native';
 
-import { Icon, List, ListItem, Card, FormInput, FormLabel, FormValidationMessage } from 'react-native-elements';
+import { Icon, List, ListItem, Card, FormInput, FormLabel, FormValidationMessage, Button } from 'react-native-elements';
 
 export default class Texts extends React.Component {
 
   state = {
-    addText: false,
-    formData: {},
+    addText: true,
+    formData: {
+      title: null,
+      text: null
+    },
     texts: [
       {
         "title": "pariatur",
-        "story": "Pariatur ex adipisicing incididunt duis consectetur cupidatat cupidatat consectetur pariatur reprehenderit deserunt quis. Qui non reprehenderit minim consectetur. Cupidatat consequat adipisicing eu aliqua et do duis eu. Elit est amet eu incididunt officia laborum duis anim nulla et in tempor excepteur.\r\n"
+        "text": "Pariatur ex adipisicing incididunt duis consectetur cupidatat cupidatat consectetur pariatur reprehenderit deserunt quis. Qui non reprehenderit minim consectetur. Cupidatat consequat adipisicing eu aliqua et do duis eu. Elit est amet eu incididunt officia laborum duis anim nulla et in tempor excepteur.\r\n"
       },
       {
         "title": "consectetur",
-        "story": "Sunt culpa enim labore ex sunt id adipisicing pariatur occaecat consectetur. Est ad ad sunt deserunt incididunt aliquip tempor sunt do adipisicing laboris id sit. Mollit et nostrud adipisicing deserunt adipisicing nostrud nisi est pariatur sunt. Incididunt officia sunt voluptate laborum laboris non cupidatat fugiat cillum magna quis. Exercitation fugiat do officia qui fugiat sit Lorem sint reprehenderit esse.\r\n"
+        "text": "Sunt culpa enim labore ex sunt id adipisicing pariatur occaecat consectetur. Est ad ad sunt deserunt incididunt aliquip tempor sunt do adipisicing laboris id sit. Mollit et nostrud adipisicing deserunt adipisicing nostrud nisi est pariatur sunt. Incididunt officia sunt voluptate laborum laboris non cupidatat fugiat cillum magna quis. Exercitation fugiat do officia qui fugiat sit Lorem sint reprehenderit esse.\r\n"
       },
       {
         "title": "duis",
-        "story": "Sunt nostrud officia laboris nostrud ad ex elit cupidatat eiusmod sint reprehenderit qui. Ex enim irure do consequat aute magna. Velit ex nulla aliqua aliqua tempor incididunt sint nulla. Ut amet tempor pariatur et amet sunt id consequat voluptate elit.\r\n"
+        "text": "Sunt nostrud officia laboris nostrud ad ex elit cupidatat eiusmod sint reprehenderit qui. Ex enim irure do consequat aute magna. Velit ex nulla aliqua aliqua tempor incididunt sint nulla. Ut amet tempor pariatur et amet sunt id consequat voluptate elit.\r\n"
       },
       {
         "title": "consequat",
-        "story": "Non ea occaecat sunt non ea velit ex. Consectetur ullamco occaecat velit eu esse sit proident proident ex irure irure laborum. Aute nulla laborum velit ullamco dolore esse aute reprehenderit. Cillum nostrud id sit laborum fugiat dolore aute fugiat.\r\n"
+        "text": "Non ea occaecat sunt non ea velit ex. Consectetur ullamco occaecat velit eu esse sit proident proident ex irure irure laborum. Aute nulla laborum velit ullamco dolore esse aute reprehenderit. Cillum nostrud id sit laborum fugiat dolore aute fugiat.\r\n"
       },
       {
         "title": "enim",
-        "story": "Proident id labore qui ullamco dolore labore irure culpa Lorem laboris consequat. Id ut mollit nostrud deserunt aute ipsum consectetur occaecat et deserunt sit commodo tempor aliqua. Commodo duis est sit duis ut commodo enim eiusmod occaecat sunt aliquip laborum esse ullamco. Irure aliqua et officia ex cupidatat proident ullamco do enim eiusmod dolore. Ullamco nostrud incididunt laborum irure dolor.\r\n"
+        "text": "Proident id labore qui ullamco dolore labore irure culpa Lorem laboris consequat. Id ut mollit nostrud deserunt aute ipsum consectetur occaecat et deserunt sit commodo tempor aliqua. Commodo duis est sit duis ut commodo enim eiusmod occaecat sunt aliquip laborum esse ullamco. Irure aliqua et officia ex cupidatat proident ullamco do enim eiusmod dolore. Ullamco nostrud incididunt laborum irure dolor.\r\n"
       },
       {
         "title": "cupidatat",
-        "story": "Ut anim aute qui dolore cupidatat magna nisi consequat excepteur. Sit occaecat amet nulla exercitation dolor ullamco voluptate consequat mollit nulla culpa. Nostrud laborum deserunt Lorem officia eu ea labore velit. Esse dolore anim amet duis esse aliquip non aute veniam est. Tempor dolore eiusmod mollit occaecat quis.\r\n"
+        "text": "Ut anim aute qui dolore cupidatat magna nisi consequat excepteur. Sit occaecat amet nulla exercitation dolor ullamco voluptate consequat mollit nulla culpa. Nostrud laborum deserunt Lorem officia eu ea labore velit. Esse dolore anim amet duis esse aliquip non aute veniam est. Tempor dolore eiusmod mollit occaecat quis.\r\n"
       },
       {
         "title": "ipsum",
-        "story": "Sint sunt veniam est aliqua amet anim minim ipsum eu eiusmod. Reprehenderit nostrud nulla voluptate pariatur magna culpa dolor mollit voluptate. Et incididunt anim sit enim ipsum aliquip aliqua do reprehenderit magna ea cillum.\r\n"
+        "text": "Sint sunt veniam est aliqua amet anim minim ipsum eu eiusmod. Reprehenderit nostrud nulla voluptate pariatur magna culpa dolor mollit voluptate. Et incididunt anim sit enim ipsum aliquip aliqua do reprehenderit magna ea cillum.\r\n"
       },
       {
         "title": "esse",
-        "story": "Excepteur ex quis ex mollit incididunt non. Sint magna id magna reprehenderit cillum elit esse Lorem ullamco pariatur aute elit. Aliquip do nisi irure exercitation sint officia aute amet nostrud duis non. Nulla minim esse est id sunt Lorem cupidatat commodo excepteur officia nulla in eiusmod eu. Aliqua exercitation duis elit fugiat fugiat in incididunt voluptate fugiat ea aute eu cupidatat. Sint cillum non sit et consectetur est minim sit occaecat aliqua officia culpa. Aute irure eiusmod cupidatat excepteur do ullamco dolore est fugiat non exercitation occaecat.\r\n"
+        "text": "Excepteur ex quis ex mollit incididunt non. Sint magna id magna reprehenderit cillum elit esse Lorem ullamco pariatur aute elit. Aliquip do nisi irure exercitation sint officia aute amet nostrud duis non. Nulla minim esse est id sunt Lorem cupidatat commodo excepteur officia nulla in eiusmod eu. Aliqua exercitation duis elit fugiat fugiat in incididunt voluptate fugiat ea aute eu cupidatat. Sint cillum non sit et consectetur est minim sit occaecat aliqua officia culpa. Aute irure eiusmod cupidatat excepteur do ullamco dolore est fugiat non exercitation occaecat.\r\n"
       },
       {
         "title": "minim",
-        "story": "Magna eu Lorem ad ea nisi deserunt elit sit velit do anim est. Laboris id minim elit ea nisi Lorem velit. Amet ullamco amet nisi laborum sint reprehenderit tempor mollit elit elit incididunt. Ad nulla eu esse commodo.\r\n"
+        "text": "Magna eu Lorem ad ea nisi deserunt elit sit velit do anim est. Laboris id minim elit ea nisi Lorem velit. Amet ullamco amet nisi laborum sint reprehenderit tempor mollit elit elit incididunt. Ad nulla eu esse commodo.\r\n"
       }
     ]
   }
 
+  formDataAddTitle(title) {
+    formData = this.state.formData
+
+    formData.title = title
+
+    this.setState({ formData })
+  }
+
+  formDataAddText(text) {
+    formData = this.state.formData
+
+    formData.text = text
+
+    this.setState({ formData })
+  }
+
+  submitFormData() {
+    texts = this.state.texts
+
+    texts.push(this.state.formData)
+
+    this.setState({
+      texts,
+      formData: {
+        title: null,
+        text: null
+      },
+      addText: false
+    })
+  }
+
   render() {
-    console.log(this.state)
     return (
       <View style={styles.container}>
         <ScrollView>
@@ -61,12 +94,17 @@ export default class Texts extends React.Component {
                   title={element.title}
                   onPress={() => this.setState()}
                 />
+
+                /*
+                  TODO:
+                    Open Reader view when ListItem is clicked on
+                */
               )
             }
           </List>
         </ScrollView>
 
-        <View style={styles.button}>
+        <View style={styles.fab}>
           <Icon
             reverse
             raised
@@ -77,11 +115,22 @@ export default class Texts extends React.Component {
 
         {this.state.addText?
           <Card containerStyle={styles.popup} title="Add text">
+              {
+                /*
+                  TODO:
+                    Exiting focus on FormInput is finnicky
+                    Only display validation message when needed
+                */
+              }
+
               <FormLabel>Title</FormLabel>
-              <FormInput inputStyle={{width: undefined}}/>
+              <FormInput inputStyle={{width: undefined}} onChangeText={this.formDataAddTitle.bind(this)} />
               <FormValidationMessage>A title is required</FormValidationMessage>
+
               <FormLabel>Text</FormLabel>
-              <FormInput multiline inputStyle={{width: undefined}}/>
+              <FormInput multiline inputStyle={{width: undefined}} onChangeText={this.formDataAddText.bind(this)} />
+
+              <Button buttonStyle={styles.popupButton} title="Submit" onPress={this.submitFormData.bind(this)} />
           </Card>
         :
           null
@@ -97,18 +146,26 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'stretch'
   },
-  button: {
+  fab: {
     position: 'absolute',
     right: 10,
     bottom: 10
   },
   popup: {
-    zIndex: 10,
-    flex: 1,
     position: 'absolute',
     top: 0,
     bottom: 10,
     left: 0,
-    right: 0
+    right: 0,
+    alignItems: 'stretch'
   },
+  popupButton: {
+    // TODO:
+    //    how to get Button to bottom of card?
+    borderRadius: 0, 
+    marginLeft: 0, 
+    marginRight: 0, 
+    marginBottom: 0,
+    marginTop: 10
+  }
 });
