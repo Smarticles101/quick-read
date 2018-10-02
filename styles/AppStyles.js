9c5f7cbd93ff1c159ae68a64cc39d9503876166d
Reetik Patel <Reetik2000@yahoo.com>
2018-10-01 21:25:12 -0500

    :zap::recycle: Moved App.js styles to external stylesheet

---
+++ styles/AppStyles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const AppStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'stretch',
+        justifyContent: 'center',
+    },
+    header: {
+        color: '#fff',
+    }
+});
+
+export default AppStyles;
