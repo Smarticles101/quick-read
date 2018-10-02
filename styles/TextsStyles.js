13c1c10ee051a89a002ddd7005c4a598c31c6e44
Reetik Patel <Reetik2000@yahoo.com>
2018-10-01 21:33:32 -0500

    :zap::recycle: Moved styles in Texts.js to external stylesheet

---
+++ styles/TextsStyles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+const TextsStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    fabRight: {
+      position: 'absolute',
+      right: 10,
+      bottom: 10
+    },
+    fabLeft: {
+      position: 'absolute',
+      left: 10,
+      bottom: 10
+    },
+    popup: {
+      position: 'absolute',
+      top: 0,
+      bottom: 10,
+      left: 0,
+      right: 0,
+    }
+  });
+
+  export default TextsStyles;
